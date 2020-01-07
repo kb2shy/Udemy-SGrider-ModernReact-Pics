a3324fec868f15ddc8b57f1fdf04ed489e152eec
Chhaian Pin <chhaianpin@hotmail.com>
2020-01-07 14:29:48 -0800

    added ImageList component to render images gathered from Unsplash

---
+++ src/components/ImageList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    console.log(props.images);
+    const images = props.images.map((image) => {
+        return <img src={image.urls.regular} key={image.id} alt={image.alt}></img>
+    })
+
+    return (
+        <div>{images}</div>
+    )
+}
+
+export default ImageList;
